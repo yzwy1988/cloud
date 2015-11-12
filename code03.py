f2b5389e614b3fda3db7410ef7413e9b217ff937
Shen WanDong <80470335@qq.com>
2015-11-12 14:45:22 +0800

    pycode

---
+++ code03.py
@@ -0,0 +1,14 @@
+x =8
+if 0<x<10:
+    print '\u%d tadeshi'%x
+a =2
+if 0<a<10:
+    print "a is a posr"
+
+def return_root(x):
+    if x<=0:
+        print "positive numbers only ,please!"
+        return 
+
+result =x**5
+print "the rquest root of x  is ",result
