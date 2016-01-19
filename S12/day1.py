2f216ae9d774c1d5f8f11f2556e35681ea299fd3
shen <80470335@qq.com>
2016-01-19 21:18:44 +0800

    "uodate"

--- S12/day1.py
+++ S12/day1.py
@@ -1,7 +1,20 @@
 # coding=utf-8
+# /usr/bin/env python
 
+lucky_num = 19
+input_num = -1
 
-# python3的语法
-print ("hello world")
+guess_count = 0
 
-print " hello world "
+while guess_count < 3:
+    input_num = int(input("Input the guess num:"))
+    if input_num > lucky_num:
+        print("the real numer is smaller.")
+    elif input_num < lucky_num:
+        print("the real numer is bigger..")
+    else:
+        print("bingo")
+        break
+    guess_count += 1
+else:
+    print("猜错了")
