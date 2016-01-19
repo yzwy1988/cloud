1966aa63c8eeee7ce78e9b6df4484bd4209ba3cb
shen <80470335@qq.com>
2016-01-19 22:00:07 +0800

    'test'

--- S12/guess_luckynum.py
+++ S12/guess_luckynum.py
@@ -6,6 +6,7 @@ input_num = -1
 
 guess_count = 0
 
+# 加上次数限制
 while guess_count < 3:
     input_num = int(input("Input the guess num:"))
     if input_num > lucky_num:
--- S12/string_format.py
+++ S12/string_format.py
@@ -4,5 +4,5 @@
 name = input("name")
 age = input("age")
 job = input("job")
-
+# 格式化输出
 print("information of %s:\n name:%s\n age:%s\n job:%s" % (name, name, age, job))
