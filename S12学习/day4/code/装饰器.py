6e87f26497072f41b20c1b0696e5605a52987c50
shen <80470335@qq.com>
2016-02-22 18:34:59 +0800

    up

--- S12学习/day4/code/装饰器.py
+++ S12学习/day4/code/装饰器.py
@@ -90,3 +90,120 @@ def move(name):
 
 
 home("Yamaha", "Super")
+
+
+# 函数是第一类对象
+def fun(a, b):
+    return (a + b)
+
+
+add = fun  # 第一种方式：add = fun，fun函数赋值给add变量
+
+
+# print(add(1,2))  add（）传参数求值return和fun啊（）一样
+# print(fun(1,2))
+def caller_func(f):
+    return f(1, 2)
+
+
+if __name__ == "__main__":
+    print(caller_func(fun))
+
+''' 将函数对象作为参数，传递给另一个函数
+可以看到func作为参数传递给caller_func函数，传参过程类似于一个赋值操作f=fun；
+于是fun函数对象，被caller_fun函数作用域中的局部变量f引用，f实际指向了函数fun；
+当执行return f（1，2）的时候，相当于执行了 return fun（1，2）；因此输出结果是3
+'''
+
+'''
+函数对象vs函数调用
+无论是把函数复制给新的标识符，还是作为参数传递给新的函数，针对的都是函数本身，而不是函数的调用
+用一个更加简单，但从外观上看，更容易产生混淆的例子来说明这个问题，例如定义了下面这个函数：
+'''
+def func():
+    return "hello world"
+
+# 然后分别执行两次赋值
+ref1 = func  # 将函数对象赋值给ref1
+ref2 = func()   #调用函数，将函数的返回值（“hello world”）赋值给ref2
+
+# 很多初学者都会混淆这两种赋值，通过python内建的type函数，可以查看一下这两次赋值的结果
+print(type(ref1))    # 返回类型function
+print(type(ref2))    # 返回类型str
+# 可以看到，ref1引用了函数对象本身，而ref2则引用了函数的返回值，通过内建的
+# callable函数，可以进一步验证ref1是可调用的，而ref2是不可调用的
+print(callable(ref1))    # True
+print(callable(ref2))    # False
+# 传参的效果与之类似
+
+
+# 闭包&LEGB法则
+# 所谓闭包，就是将组成函数的语句和这些语句的执行环境打包在一起时，得到的对象
+
+# 听上去的确有些复杂，还是用一个例子来帮助理解一下，假设我们在foo.py模块中做了如下定义：
+filename = "foo.py"
+def call_func(f):
+    return f()  # 如前面介绍的，f引用一个函数对象，然后调用它
+
+
+# 在另一个func.py模块中，写下了这样的代码：
+import foo
+
+filename = "func.py"
+
+def show_filename():
+    return "filename:%s" % filename
+if __name__ == "__name__":
+    print(foo.call_func(show_filename))  # 注意：实际发生调用的位置，是在foo.call_func函数中
+# 当我们用python func.py命令执行func.py时输出结果为：
+# python func.py
+# 很显然show_filename（）函数使用的filename变量的值，是在与它相同环境（func.py模块）中定义的那个。尽管
+# foo.py模块中也定义了同名的filename变量，而且实际调用show_filename的位置也是在foo.py的call_func内部
+
+# 而对于嵌套函数，这一机制则会表现的更加明显：闭包将会捕捉内层函数执行所需的整个环境：
+
+# 而对于嵌套函数,这一机制则会表现的更加明显：闭包将会捕捉内层函数执行所需的整个环境:
+
+# enclosed.py
+import foo
+def wrapper():
+    fileename ="enclosed.py"
+    def show_filename():
+        return "filename: %s"%filename
+    print(foo.call_func(show_filename))    # 输出:filename:enclosed.py
+
+# 实际上，每一个函数对象，都有一个指向了该函数定义时所在全局名称空间的__globals__属性：
+著作权归作者所有。
+商业转载请联系作者获得授权，非商业转载请注明出处。
+作者：陈伟
+链接：https://www.zhihu.com/question/25950466/answer/31731502
+来源：知乎
+'''
+#show_filename inside wrapper
+#show_filename.__globals__
+{
+'__builtins__': <module '__builtin__' (built-in)>,        #内建作用域环境
+'__file__': 'enclosed.py',
+'wrapper': <function wrapper at 0x7f84768b6578>,      #直接外围环境
+'__package__': None,
+'__name__': '__main__',
+'foo': <module 'foo' from '/home/chiyu/foo.pyc'>,         #全局环境
+'__doc__': None
+}'''
+
+# 当代码执行到show_filename中的return "filename：%s"%filename语句时，解析器按照下列的顺序查找filename变量：
+# local -本地函数（show_filename）内部，通过任何方式赋值的，而且没有被global关键字声明为全局变量的filename变量;
+# Enclosing -直接外围空间（上层函数wrapper）的本地作用域，查找filename变量（如果有多层嵌套，则由内而外逐层查找，直至最外层的函数）；
+# Global - 全局空间（模块enclosed.py），在模块顶层复制的filename变量；
+# Builtin - 内置模块（__builtin__)中预定义的变量名中查找filename变量；
+# 在任何一层先找到符合要求的filename变量，则不在像更外层查找。如果直到Builtin层仍然没有找到符合要求的变量，则抛出NameError异常。
+# 这就是变量名解析的：LEGB法则
+
+# 总结：
+# 1.闭包最重要的使用价值在于：封存函数执行的上下文环境；
+# 2.闭包在其捕捉的执行环境（def语句块所在上下文）中，也遵循LEGB规则逐层查找，直至找到符合要求的变量，或者抛出异常。
+
+
+# 装饰器&语法糖（syntax sugar）
+# 那么闭包和装饰器又有什么关系呢？
+# 上问题到闭包的重要特性：封存上下文，这一特可以巧妙的被用于现有函数的包装，从而为现有函数更加功能。而这就是装饰器。
--- 个人练习/code16.py
+++ 个人练习/code16.py
@@ -1,87 +1,101 @@
 # -*- coding: utf-8 -*-
 
 
-import  wx
-import  wx.html 
+import wx
+import wx.html
 import urllib
 import thread
 import re
-import sys 
-reload(sys) 
-sys.setdefaultencoding('utf8') 
-#----------------------------------------------------------------------
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+# ----------------------------------------------------------------------
 
 class TestFrame(wx.Frame):
-    def __init__(self, parent,):
+    def __init__(self, parent, ):
         wx.Frame.__init__(self, parent, -1, "天气预报",
-                         style =
-                           wx.FRAME_SHAPED
-                         | wx.SIMPLE_BORDER
-                         | wx.FRAME_NO_TASKBAR
-                         )
-        self.count=0
-        self.weatherInfo=''#天气信息
+                          style=
+                          wx.FRAME_SHAPED
+                          | wx.SIMPLE_BORDER
+                          | wx.FRAME_NO_TASKBAR
+                          )
+        self.count = 0
+        self.weatherInfo = ''  # 天气信息
         self.hasShape = False
-        self.Bind(wx.EVT_LEFT_DOWN,self.OnLeftDown)
-        self.Bind(wx.EVT_LEFT_UP,self.OnLeftUp)
-        self.Bind(wx.EVT_MOTION,self.OnMouseMove)
-        self.delta = (0,0)
-        self.bmp=wx.Image('bg.png',wx.BITMAP_TYPE_PNG,-1).ConvertToBitmap()
-        self.w, self.h = self.bmp.GetWidth(), self.bmp.GetHeight()#获取图片背景长宽
-        self.bmp.SetMask(wx.Mask(self.bmp, wx.BLACK))   
-        self.SetClientSize( (self.w,self.h) )
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+        self.Bind(wx.EVT_MOTION, self.OnMouseMove)
+        self.delta = (0, 0)
+        self.bmp = wx.Image('bg.png', wx.BITMAP_TYPE_PNG, -1).ConvertToBitmap()
+        self.w, self.h = self.bmp.GetWidth(), self.bmp.GetHeight()  # 获取图片背景长宽
+        self.bmp.SetMask(wx.Mask(self.bmp, wx.BLACK))
+        self.SetClientSize((self.w, self.h))
         self.SetWindowShape()
-        self.html= wx.html.HtmlWindow(self,-1,(self.w-230,50),(200,150))#天气信息显示区域
-        self.initCloseButton()#初始化关闭按键
-        self.initimg()#初始化天气图片
-        self.Bind(wx.EVT_PAINT,self.OnPaint)
-        thread.start_new_thread(self.getWeatherInfo,(1,2))#开启获取天气线程
+        self.html = wx.html.HtmlWindow(self, -1, (self.w - 230, 50), (200, 150))  # 天气信息显示区域
+        self.initCloseButton()  # 初始化关闭按键
+        self.initimg()  # 初始化天气图片
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        thread.start_new_thread(self.getWeatherInfo, (1, 2))  # 开启获取天气线程
+
     def initCloseButton(self):
-        self.closebmp=wx.Image('no.png',wx.BITMAP_TYPE_PNG,-1).ConvertToBitmap()
-        self.closebmp.SetMask(wx.Mask(self.closebmp,wx.BLACK))
-        button=wx.BitmapButton(self, -1,self.closebmp,style=wx.NO_BORDER,pos=(self.w-30,20))   
+        self.closebmp = wx.Image('no.png', wx.BITMAP_TYPE_PNG, -1).ConvertToBitmap()
+        self.closebmp.SetMask(wx.Mask(self.closebmp, wx.BLACK))
+        button = wx.BitmapButton(self, -1, self.closebmp, style=wx.NO_BORDER, pos=(self.w - 30, 20))
         button.SetDefault()
-        self.Bind(wx.EVT_BUTTON, self.onButtonClick,button)
-    def getWeatherInfo(self,no,interval):
-            web=urllib.urlopen('http://m.weather.com.cn/data/101190101.html')
-            self.weatherInfo=web.read()
-            web.close()
-            print self.weatherInfo
-            self.updateText()
+        self.Bind(wx.EVT_BUTTON, self.onButtonClick, button)
+
+    def getWeatherInfo(self, no, interval):
+        web = urllib.urlopen('http://m.weather.com.cn/data/101190101.html')
+        self.weatherInfo = web.read()
+        web.close()
+        print
+        self.weatherInfo
+        self.updateText()
+
     def updateText(self):
-        weatherinfo=eval(self.weatherInfo)['weatherinfo']
+        weatherinfo = eval(self.weatherInfo)['weatherinfo']
         for i in weatherinfo:
-            print i,weatherinfo[i]
-        city=weatherinfo['city'].decode('utf-8')
-        print city
-        strs='<font  color="#444444">城市——%s</font>&nbsp;&nbsp;&nbsp;&nbsp;<font   color="red" size="1">%s</font><br> \
+            print
+            i, weatherinfo[i]
+        city = weatherinfo['city'].decode('utf-8')
+        print
+        city
+        strs = '<font  color="#444444">城市——%s</font>&nbsp;&nbsp;&nbsp;&nbsp;<font   color="red" size="1">%s</font><br> \
              <font color="#444444">气温：%s<br>天气：%s</font><br>\
              <font color="#444444">风向：%s</font><br><br>\
-             <font color="#444444">  </font>'%(weatherinfo['city'].decode('utf-8')
-                                               ,weatherinfo['week'].decode('utf-8')
-                                               ,weatherinfo['temp1'].decode('utf-8')
-                                               ,weatherinfo['weather1'].decode('utf-8')
-                                               ,weatherinfo['wind1'].decode('utf-8'))
+             <font color="#444444">  </font>' % (weatherinfo['city'].decode('utf-8')
+                                                                , weatherinfo['week'].decode('utf-8')
+                                                                , weatherinfo['temp1'].decode('utf-8')
+                                                                , weatherinfo['weather1'].decode('utf-8')
+                                                                , weatherinfo['wind1'].decode('utf-8'))
         self.html.SetPage(strs)
-        imgno= weatherinfo['img2']
-        imgno=re.findall(r'\d',imgno)
-        print imgno[0]
-        self.bmp2=wx.Image('images/w%s.png'%imgno[0],wx.BITMAP_TYPE_PNG,-1).ConvertToBitmap()  #更改天气图片 
-        self.Refresh()#刷新窗口
+        imgno = weatherinfo['img2']
+        imgno = re.findall(r'\d', imgno)
+        print
+        imgno[0]
+        self.bmp2 = wx.Image('images/w%s.png' % imgno[0], wx.BITMAP_TYPE_PNG, -1).ConvertToBitmap()  # 更改天气图片
+        self.Refresh()  # 刷新窗口
+
     def initimg(self):
-        self.bmp2=wx.Image('images/w0.png',wx.BITMAP_TYPE_PNG,-1).ConvertToBitmap()
+        self.bmp2 = wx.Image('images/w0.png', wx.BITMAP_TYPE_PNG, -1).ConvertToBitmap()
+
     def SetWindowShape(self, *evt):
         r = wx.RegionFromBitmap(self.bmp)
         self.hasShape = self.SetShape(r)
+
     def OnPaint(self, evt):
         dc = wx.PaintDC(self)
-        dc.DrawBitmap(self.bmp, 0,0, True)
+        dc.DrawBitmap(self.bmp, 0, 0, True)
         dc.DrawBitmap(self.bmp2, 10, 20, True)
-    def onButtonClick(self,evt):
-        print '按键点击'
+
+    def onButtonClick(self, evt):
+        print
+        '按键点击'
         self.Destroy()
- 
-   
+
     def OnLeftDown(self, evt):
         self.CaptureMouse()
         x, y = self.ClientToScreen(evt.GetPosition())
@@ -89,19 +103,21 @@ class TestFrame(wx.Frame):
         dx = x - originx
         dy = y - originy
         self.delta = ((dx, dy))
+
     def OnLeftUp(self, evt):
         if self.HasCapture():
             self.ReleaseMouse()
+
     def OnMouseMove(self, evt):
         if evt.Dragging() and evt.LeftIsDown():
             x, y = self.ClientToScreen(evt.GetPosition())
             fp = (x - self.delta[0], y - self.delta[1])
-            self.Move(fp)     
- 
-#----------------------------------------------------------------------
-app=wx.PySimpleApp()
+            self.Move(fp)
+
+            # ----------------------------------------------------------------------
+
+
+app = wx.PySimpleApp()
 win = TestFrame(None)
 win.Show(True)
 app.MainLoop()
- 
-
